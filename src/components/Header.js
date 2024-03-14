3bed67fa06f0865fdf183ad3fc52fa9e5f6e2c72
Sinchanarani <sinchanaranig123@gmail.com>
2024-03-14 12:26:39 +0530

    happyJourney

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="container">
-      <div className="title">Plan A Tour</div>
+      <div className="title">Discover</div>
     </div>
   );
 };
